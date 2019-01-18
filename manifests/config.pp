04c3c916ecf78065e0b20c6dd54e9e1f2a09b531
root <root@criticalhojo1c.mylabserver.com>
2019-01-18 20:28:27 +0000

    fixed spelling of hasrestart from init.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,7 +8,7 @@ class nginx::config (
  String $log_dir       = $nginx::config_log_dir,
  String $pid_file      = $nginx::config_pid_file,
  Optional[String] $vdir_enable   = $nginx::config_vdir_enable,
- String $process_user  = $nginx::process_user,
+ String $process_user  = $nginx::config_process_user,
  String $docroot       = $nginx::docroot,
 ) {
   file { 'nginx_conf': 
